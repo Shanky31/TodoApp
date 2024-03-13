b2611c756d8b6d4ff6a420b372c2fe0dadbd8179
Tshrvrma <tushartv123@gmail.com>
2024-03-13 21:22:41 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+
+
+<template>
+  <div id="app">
+    <TodoApp />
+  </div>
+</template>
+
+<script>
+import TodoApp from './components/TodoApp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TodoApp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  background-color: #29c7dc;
+  width: auto;
+  min-height: 100vh; 
+  overflow:hidden;
+}
+todo-app {
+  height: auto; 
+}
+</style>
+
